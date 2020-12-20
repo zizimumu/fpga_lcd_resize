4c7b9019610caf36e66b5b2fafa7dbb8c93c7cc9
zizimumu <jiequnx@foxmail.com>
2020-12-20 18:49:17 +0800

    write/read test ok

---
+++ pcie_test.v
@@ -0,0 +1,119 @@
+module pcie_test
+(
+    input              i_sys_rst_n        ,
+    input              i_fpga_clk_50m     ,
+	 output [3:0]   		  o_fpga_led,
+	 output wire [11:0]	sd_a,
+	 output wire [1:0]	sd_ba,
+	 output wire 			sd_cas,
+	 output wire			sd_cs,
+	 inout wire [63:0]	sd_dq,
+	 output wire [7:0]	sd_dqm,
+	 output wire			sd_ras,
+	 output wire			sd_we,
+	 output wire 			sd_clk
+);
+
+
+
+
+wire clk_100m_out;
+wire clk_50m_out;
+wire clk_100m_ctl;
+wire        locked;
+
+wire        wr_en;                          //SDRAM 写端口:写使能
+wire [63:0] wr_data;                        //SDRAM 写端口:写入的数据
+wire        rd_en;                          //SDRAM 读端口:读使能
+wire [63:0] rd_data;                        //SDRAM 读端口:读出的数据
+wire        sdram_init_done;                //SDRAM 初始化完成信号
+
+wire        sys_rst_n;                      //系统复位信号
+wire        error_flag;                     //读写测试错误标志
+wire [3:0]		cycle_countor;				// 测试周期计数
+
+`define		SDRAM_TEST_LEN		    24'h400000 
+
+
+assign sys_rst_n = i_sys_rst_n & locked;
+
+clk_pll	clk_pll_inst (
+	.areset ( ~i_sys_rst_n ),
+	.inclk0 ( i_fpga_clk_50m ),
+	.c0 ( clk_100m_out ),
+	.c1 ( clk_100m_ctl ),
+	.locked (locked)
+	);
+
+
+//SDRAM测试模块，对SDRAM进行读写测试
+sdram_test #(.TEST_LEN(`SDRAM_TEST_LEN))
+u_sdram_test(
+    .clk_50m            (i_fpga_clk_50m),
+    .rst_n              (sys_rst_n),
+    
+    .wr_en              (wr_en),
+    .wr_data            (wr_data),
+    .rd_en              (rd_en),
+    .rd_data            (rd_data),   
+    
+    .sdram_init_done    (sdram_init_done),    
+    .error_flag         (error_flag),
+	.cycle_countor		(cycle_countor)
+    );
+
+//利用LED灯指示SDRAM读写测试的结果
+led_disp u_led_disp(
+    .clk_50m            (i_fpga_clk_50m),
+    .rst_n              (sys_rst_n),
+   
+    .error_flag         (error_flag),
+    .led                (o_fpga_led),
+	.cycle_countor		(cycle_countor)
+    );
+
+//SDRAM 控制器顶层模块,封装成FIFO接口
+//SDRAM 控制器地址组成: {bank_addr[1:0],row_addr[12:0],col_addr[8:0]}
+sdram_top u_sdram_top(
+	.ref_clk			(clk_100m_ctl),			//sdram	控制器参考时钟
+	.out_clk			(clk_100m_out),	//用于输出的相位偏移时钟
+	.rst_n				(sys_rst_n),		//系统复位
+    
+    //用户写端口
+	.wr_clk 			(i_fpga_clk_50m),		    //写端口FIFO: 写时钟
+	.wr_en				(wr_en),			//写端口FIFO: 写使能
+	.wr_data		    (wr_data),		    //写端口FIFO: 写数据
+	.wr_min_addr		(24'd0),			//写SDRAM的起始地址
+	.wr_max_addr		(`SDRAM_TEST_LEN),		    //写SDRAM的结束地址
+	.wr_len			    (10'd256),			//写SDRAM时的数据突发长度
+	.wr_load			(~sys_rst_n),		//写端口复位: 复位写地址,清空写FIFO
+   
+    //用户读端口
+	.rd_clk 			(i_fpga_clk_50m),			//读端口FIFO: 读时钟
+    .rd_en				(rd_en),			//读端口FIFO: 读使能
+	.rd_data	    	(rd_data),		    //读端口FIFO: 读数据
+	.rd_min_addr		(24'd0),			//读SDRAM的起始地址
+	.rd_max_addr		(`SDRAM_TEST_LEN),	    	//读SDRAM的结束地址
+	.rd_len 			(10'd256),			//从SDRAM中读数据时的突发长度
+	.rd_load			(~sys_rst_n),		//读端口复位: 复位读地址,清空读FIFO
+	   
+     //用户控制端口  
+	.sdram_read_valid	(1'b1),             //SDRAM 读使能
+	.sdram_init_done	(sdram_init_done),	//SDRAM 初始化完成标志
+   
+	//SDRAM 芯片接口
+	.sdram_clk			(sd_clk),        //SDRAM 芯片时钟
+	.sdram_cke			(),        //SDRAM 时钟有效
+	.sdram_cs_n			(sd_cs),       //SDRAM 片选
+	.sdram_ras_n		(sd_ras),      //SDRAM 行有效
+	.sdram_cas_n		(sd_cas),      //SDRAM 列有效
+	.sdram_we_n			(sd_we),       //SDRAM 写有效
+	.sdram_ba			(sd_ba),         //SDRAM Bank地址
+	.sdram_addr			(sd_a),       //SDRAM 行/列地址
+	.sdram_data			(sd_dq),       //SDRAM 数据
+	.sdram_dqm			(sd_dqm)         //SDRAM 数据掩码
+    );
+
+	 
+
+endmodule
---
+++ sdram/sdram_test.v
@@ -0,0 +1,144 @@
+//****************************************Copyright (c)***********************************//
+//技术支持：www.openedv.com
+//淘宝店铺：http://openedv.taobao.com 
+//关注微信公众平台微信号："正点原子"，免费获取FPGA & STM32资料。
+//版权所有，盗版必究。
+//Copyright(C) 正点原子 2018-2028
+//All rights reserved                               
+//----------------------------------------------------------------------------------------
+// File name:           sdram_test
+// Last modified Date:  2018/3/18 8:41:06
+// Last Version:        V1.0
+// Descriptions:        SDRAM读写测试: 向SDRAM中写入数据,然后将数据读出,并判断读出的数据是否正确
+//----------------------------------------------------------------------------------------
+// Created by:          正点原子
+// Created date:        2018/3/18 8:41:06
+// Version:             V1.0
+// Descriptions:        The original version
+//
+//----------------------------------------------------------------------------------------
+//****************************************************************************************//
+
+module sdram_test#(
+	parameter TEST_LEN = 32'd1024,  // 测试数据长度
+	parameter TEST_CYCLE = 32'd50_000000 // 测试周期, 50000000为1s
+) 
+(
+    input             clk_50m,          //时钟
+    input             rst_n,            //复位,低有效
+    
+    output reg        wr_en,            //SDRAM 写使能
+    output reg [63:0] wr_data,          //SDRAM 写入的数据
+    output reg        rd_en,            //SDRAM 读使能
+    input      [63:0] rd_data,          //SDRAM 读出的数据
+    
+    input             sdram_init_done,  //SDRAM 初始化完成标志
+    output reg        error_flag,        //SDRAM 读写测试错误标志
+	output reg [3:0] cycle_countor
+    );
+
+	
+//reg define
+reg        init_done_d0;                //寄存SDRAM初始化完成信号
+reg        init_done_d1;                //寄存SDRAM初始化完成信号
+reg [10:0] wr_cnt;                      //写操作计数器
+reg [10:0] rd_cnt;                      //读操作计数器
+reg        rd_valid;                    //读数据有效标志
+   
+//*****************************************************
+//**                    main code
+//***************************************************** 
+
+//同步SDRAM初始化完成信号
+always @(posedge clk_50m or negedge rst_n) begin
+    if(!rst_n) begin
+        init_done_d0 <= 1'b0;
+        init_done_d1 <= 1'b0;
+    end
+    else begin
+        init_done_d0 <= sdram_init_done;
+        init_done_d1 <= init_done_d0;
+    end
+end            
+
+
+
+reg [31:0] counter;
+
+always @(posedge clk_50m or negedge rst_n) begin
+    if(!rst_n) begin
+        counter <= 32'd0;  
+		
+	end
+    else if(init_done_d1 && (counter < TEST_CYCLE) ) begin
+	
+        counter <= counter + 1'b1;
+	end
+    else begin
+        counter <= 32'd0;
+	end
+end  
+  
+
+
+
+// fifo 写数据时，wr_en使能的同时要准备好数据
+always @(posedge clk_50m or negedge rst_n) begin
+    if(!rst_n) begin      
+        wr_en   <= 1'b0;
+        wr_data <= 16'd0;
+		cycle_countor <= 4'd0;
+    end
+    else if(counter >= 11'd1 && (counter <= TEST_LEN)) begin
+            wr_en   <= 1'b1;            //写使能拉高
+			rd_en   <= 1'b0;
+            wr_data <=  counter;          //写入数据1~1024 64'h55aa55aa_00000000 +
+    end    
+    else if(counter > TEST_LEN && (counter <= TEST_LEN*2)) begin
+            wr_en   <= 1'b0;            //写使能拉高
+			rd_en   <= 1'b1;
+
+    end
+	else if(counter == TEST_CYCLE) 
+		cycle_countor <= cycle_countor + 1'b1;
+	else begin
+            wr_en   <= 1'b0;            //写使能拉高
+			rd_en   <= 1'b0;	
+	end
+end  
+
+
+// fifo读数据时，在rd_en有效后下一个周期读取
+reg [31:0] rd_counter;
+//读数据有效时,若读取数据错误,给出标志信号
+
+always @(posedge clk_50m or negedge rst_n) begin
+    if(!rst_n) begin
+		rd_counter <= 32'd0;
+	end
+    else if(rd_en) begin
+		rd_counter <= rd_counter + 1'b1;
+	end
+	else begin
+		rd_counter <= 32'd0;
+	end
+		
+end
+
+
+//读出FIFO的第一个值是上次读出的值，所以要延迟一个clock再做判断
+always @(posedge clk_50m or negedge rst_n) begin
+    if(!rst_n) begin
+        error_flag <= 1'b0; 
+	end
+    else if(rd_en && rd_counter >= 32'd1) begin
+		if (rd_data != (  rd_counter) && cycle_countor >= 1)  // 因为FIFO有预读，导致第一次读到的数据不正确，这里避开第一个写读周期
+			error_flag <= 1'b1;             // 若读取的数据错误,将错误标志位拉高 
+		else
+			error_flag <= error_flag;
+		
+	end
+end
+
+
+endmodule 
